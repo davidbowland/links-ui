5129f4a82aeb37f1ed1e2cbb60a2ab25008b1930
David Bowland <dbowland1@gmail.com>
2022-03-20 10:55:31 -0500

    Fix sign in button wrapping

---
+++ src/components/auth/elements.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button'
+import styled from 'styled-components'
+
+export const SignInButton = styled(Button)({
+  borderColor: '#fff',
+  color: '#fff',
+  '&:hover': {
+    borderColor: '#ccc',
+  },
+})
